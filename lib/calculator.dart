8cb2be021547b0a43fe4220d539f008adbf4aa31
Sébastien Gruhier <sgruhier@gmail.com>
2024-05-14 14:21:27 +0200

    fix bad merge

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -21,31 +21,72 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     final size = MediaQuery.sizeOf(context);
     const margin = 4;
-    return OrientationBuilder(builder: (context, orientation) {
-      final bool isLandscape = orientation == Orientation.landscape;
-      if (isLandscape) {
-        final keySize = min(size.width, size.height) / 5 - 4 * margin;
-        return Row(
-          children: [
-            Expanded(child: keyPad(keySize)),
-            const VerticalDivider(
-              width: 16,
-              thickness: 1,
+    final keySize = min(size.width, size.height) / 4 - 4 * margin;
+    return Column(
+      children: [
+        Expanded(
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: FittedBox(
+              child: Text(
+                result,
+                style: const TextStyle(fontSize: 48),
+              ),
             ),
-            Expanded(child: display()),
-          ],
-        );
-      } else {
-        final keySize = min(size.width, size.height) / 4 - 4 * margin;
-        return Column(
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Text(
+            operation,
+            style: TextStyle(
+              fontSize: 24,
+              color: error ? Colors.red : Colors.grey,
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Row(
           children: [
-            Expanded(child: display()),
-            const SizedBox(height: 8),
-            keyPad(keySize),
+            CalculatorKey(value: "AC", onTap: clear, height: keySize),
+            CalculatorKey(value: "(", onTap: addKey, height: keySize),
+            CalculatorKey(value: ")", onTap: addKey, height: keySize),
+            CalculatorKey(value: "/", onTap: addKey, height: keySize),
           ],
-        );
-      }
-    });
+        ),
+        Row(
+          children: [
+            CalculatorKey(value: "7", onTap: addKey, height: keySize),
+            CalculatorKey(value: "8", onTap: addKey, height: keySize),
+            CalculatorKey(value: "9", onTap: addKey, height: keySize),
+            CalculatorKey(value: "*", onTap: addKey, height: keySize),
+          ],
+        ),
+        Row(
+          children: [
+            CalculatorKey(value: "4", onTap: addKey, height: keySize),
+            CalculatorKey(value: "5", onTap: addKey, height: keySize),
+            CalculatorKey(value: "6", onTap: addKey, height: keySize),
+            CalculatorKey(value: "-", onTap: addKey, height: keySize),
+          ],
+        ),
+        Row(
+          children: [
+            CalculatorKey(value: "1", onTap: addKey, height: keySize),
+            CalculatorKey(value: "2", onTap: addKey, height: keySize),
+            CalculatorKey(value: "3", onTap: addKey, height: keySize),
+            CalculatorKey(value: "+", onTap: addKey, height: keySize),
+          ],
+        ),
+        Row(
+          children: [
+            CalculatorKey(value: "0", onTap: addKey, height: keySize, flex: 2),
+            CalculatorKey(value: ".", onTap: addKey, height: keySize),
+            CalculatorKey(value: "=", onTap: compute, height: keySize),
+          ],
+        ),
+      ],
+    );
   }
 
   void addKey(String value) {
